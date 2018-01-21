a7e33a1e82923860d9474c471a8b3bd547101c85
Ivan Kalchev <ivan.kalchev@icloud.com>
2018-01-21 11:45:02 +0200

    HAP-python is now on PyPi!
    Slight changes to setup scripts and README.

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup
 
 setup(
     name="HAP-python",
-    description="HomeKit Accessory Protocol implementation in python",
+    description="HomeKit Accessory Protocol implementation in python3",
     author="Ivan Kalchev",
-    version="0.5",
+    version="1.0",
+    url="https://github.com/ikalchev/HAP-python.git",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3'
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Home Automation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
     ],
     license="Apache-2.0",
     packages=[
