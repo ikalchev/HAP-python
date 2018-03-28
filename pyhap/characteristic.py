e5556078795dfbd647d1fdb12a7fd3b4d1bc9e4c
cdce8p <30130371+cdce8p@users.noreply.github.com>
2018-03-28 17:59:15 +0200

    Merged set_value and get_hap_value
    * Small improvements
    * Added helper method '_validate_value'
    * Added helper method '_get_default_value'
    * Raise ValueError if properties are invalid
    * Added __slots__

--- pyhap/characteristic.py
+++ pyhap/characteristic.py
@@ -56,21 +56,14 @@ class CharacteristicError(Exception):
     pass
 
 
-class NotConfiguredError(Exception):
-    """Raised when an operation is attempted on a characteristic that has not been
-    fully configured.
-    """
-    pass
-
-
-_HAP_NUMERIC_FIELDS = {"maxValue", "minValue", "minStep", "unit"}
+_HAP_NUMERIC_FIELDS = ('maxValue', 'minValue', 'minStep', 'unit')
 """Fields that should be included in the HAP representation of the characteristic.
 
 That is, if they are present in the specification of a numeric-value characteristic.
 """
 
 
-class Characteristic(object):
+class Characteristic:
     """Represents a HAP characteristic, the smallest unit of the smart home.
 
     A HAP characteristic is some measurement or state, like battery status or
@@ -78,6 +71,9 @@ class Characteristic(object):
     Each characteristic has a unique type UUID and a set of properties,
     like format, min and max values, valid values and others.
     """
+
+    __slots__ = ['display_name', 'type_id', 'properties', 'broker',
+                 'setter_callback', 'value']
 
     def __init__(self, display_name, type_id, properties, value=None, broker=None):
         """Initialise with the given properties.
@@ -98,34 +94,66 @@ class Characteristic(object):
             - else use `HAP_FORMAT.DEFAULT` for the format of this characteristic.
         :type value: Depends on `properties["Format"]`
         """
-        assert "Format" in properties and "Permissions" in properties
+        if 'Format' not in properties or 'Permissions' not in properties:
+            raise ValueError('Invalid properties')
         self.display_name = display_name
         self.type_id = type_id
         self.properties = properties
-        if value:
-            self.value = value
-        else:
-            if self.properties.get('ValidValues'):
-                self.value = min(self.properties['ValidValues'].values())
-            else:
-                self.value = HAP_FORMAT.DEFAULT[properties["Format"]]
         self.broker = broker
         self.setter_callback = None
+        self.value = value or self._get_default_value()
 
     def __repr__(self):
         """Return the representation of the characteristic."""
-        return "<characteristic display_name='{}' value={} properties={}>" \
+        return '<characteristic display_name={} value={} properties={}>' \
             .format(self.display_name, self.value, self.properties)
 
+    def _get_default_value(self):
+        """Helper method. Return default value for format."""
+        if self.properties.get('ValidValues'):
+            return min(self.properties['ValidValues'].values())
+        else:
+            value = HAP_FORMAT.DEFAULT[self.properties['Format']]
+            return self._validate_value(value)
+
+    def _validate_value(self, value):
+        """Perform value validation depending on format."""
+        if self.properties.get('ValidValues'):
+            if value not in self.properties['ValidValues'].values():
+                raise ValueError
+            else:
+                return value
+        elif self.properties['Format'] == HAP_FORMAT.STRING:
+            return str(value)[:256]
+        elif self.properties['Format'] == HAP_FORMAT.NUMERIC:
+            value = min(self.properties.get('maxValue', value), value)
+            return max(self.properties.get('minValue', value), value)
+        elif self.properties['Format'] == HAP_FORMAT.BOOL:
+            return bool(value)
+        elif self.properties['Format'] == HAP_FORMAT.ARRAY:
+            # TODO: Add validation
+            pass
+        elif self.properties['Format'] == HAP_FORMAT.DICTIONARY:
+            # TODO: Add validation
+            pass
+        elif self.properties['Format'] == HAP_FORMAT.DATA:
+            # TODO: Add validation
+            pass
+        elif self.properties['Format'] == HAP_FORMAT.TLV8:
+            # TODO: Add validation
+            pass
+
+        return value
+
     def set_value(self, value, should_notify=True, should_callback=True):
-        """Set the given raw value. It is checked if it is a valid value.
+        """Set the given raw value.
 
         :param value: The value to assign as this Characteristic's value.
         :type value: Depends on properties["Format"]
 
         :param should_notify: Whether a the change should be sent to subscribed clients.
-            The notification is called _after_ the setter callback. Notify will be
-            performed if and only if the broker is set, i.e. not None.
+            The notification is called _after_ the setter callback.
+            Notify will be performed if broker is set.
         :type should_notify: bool
 
         :param should_callback: Whether to invoke the callback, if such is set. This
@@ -137,13 +165,10 @@ class Characteristic(object):
         :raise ValueError: When the value being assigned is not one of the valid values
             for this Characteristic.
         """
-        if self.properties.get('ValidValues') and \
-                value not in self.properties['ValidValues'].values():
-            raise ValueError
-        self.value = value
-        if self.setter_callback is not None and should_callback:
-            self.setter_callback(value)
-        if should_notify and self.broker is not None:
+        self.value = self._validate_value(value)
+        if self.setter_callback and should_callback:
+            self.setter_callback(self.value)
+        if self.broker and should_notify:
             self.notify()
 
     def override_properties(self, properties=None, valid_values=None):
@@ -163,43 +188,25 @@ class Characteristic(object):
         if valid_values:
             self.properties['ValidValues'] = valid_values
 
-    def get_hap_value(self):
-        """Get the value of the characteristic, constrained with the HAP properties.
-        """
-        val = self.value
-        if self.properties["Format"] == HAP_FORMAT.STRING:
-            val = val[:256]
-        elif self.properties["Format"] in HAP_FORMAT.NUMERIC:
-            if "maxValue" in self.properties:
-                val = min(self.properties["maxValue"], val)
-            if "minValue" in self.properties:
-                val = max(self.properties["minValue"], val)
-        return val
-
     def notify(self):
-        """Notify clients about a value change.
+        """Notify clients about a value change. Sends the value.
 
         .. note:: Non-blocking, i.e. does not wait for the update to be sent.
-        .. note:: Uses the `get_hap_value`, i.e. sends the HAP value.
         .. seealso:: accessory_driver.publish
 
-        :raise NotConfiguredError: When the broker is not set.
+        :raise RuntimeError: When the broker is not set.
         """
-        if self.broker is None:
-            raise NotConfiguredError("Attempted to notify when `broker` is None. "
-                                     "Consider adding the characteristic to a "
-                                     "Service and then to an Accessory.")
+        if not self.broker:
+            raise RuntimeError('Notify failed, because broker is not set')
 
         data = {
-            "type_id": self.type_id,
-            "value": self.get_hap_value(),
+            'type_id': self.type_id,
+            'value': self.value,
         }
         self.broker.publish(data, self)
 
     def to_HAP(self, iid_manager):
-        """Create a HAP representation of this Characteristic.
-
-        .. note:: Uses the `get_hap_value`, i.e. sends the HAP value.
+        """Create a HAP representation of this Characteristic. Sends the value.
 
         :param iid_manager: IID manager to query for this object's IID.
         :type iid_manager: IIDManager
@@ -221,12 +228,11 @@ class Characteristic(object):
         else:
             value_info = dict()
 
-        val = self.get_hap_value()
         if self.properties["Format"] == HAP_FORMAT.STRING:
-            if len(val) > 64:
-                value_info["maxLen"] = min(len(val), 256)
+            if len(self.value) > 64:
+                value_info["maxLen"] = min(len(self.value), 256)
         if HAP_PERMISSIONS.READ in self.properties["Permissions"]:
-            value_info["value"] = val
+            value_info["value"] = self.value
 
         hap_rep.update(value_info)
         return hap_rep
