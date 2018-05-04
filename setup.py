c045c1d95c3c00f9152a09e60cde79ba006bec19
cdce8p <30130371+cdce8p@users.noreply.github.com>
2018-05-04 10:18:55 +0200

    Version bump to 2.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="HAP-python",
     description="HomeKit Accessory Protocol implementation in python3",
     author="Ivan Kalchev",
-    version="1.1.9",
+    version="2.0.0",
     url="https://github.com/ikalchev/HAP-python.git",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
