7f14eb153252ac4706b9f1c658662e7a0982fb7e
Ivan Kalchev <ivan.kalchev@icloud.com>
2021-10-07 23:58:52 +0300

    v4.3.0

--- pyhap/const.py
+++ pyhap/const.py
@@ -1,7 +1,7 @@
 """This module contains constants used by other modules."""
 MAJOR_VERSION = 4
-MINOR_VERSION = 2
-PATCH_VERSION = 1
+MINOR_VERSION = 3
+PATCH_VERSION = 0
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
 REQUIRED_PYTHON_VER = (3, 6)
