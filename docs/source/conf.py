57e8771cd948f54704199231de4e316fbc0778f3
qqaatw <qqaatw@gmail.com>
2021-12-04 15:43:43 +0800

    Add source button to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ release = '.'.join(map(str, __version__))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    'sphinx.ext.autodoc', 'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
