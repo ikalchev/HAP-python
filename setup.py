97c4913cdf1c47e8ad60b617fcc1f5d845cee6ca
J. Nick Koston <nick@koston.org>
2022-06-27 09:27:00 -0500

    adjust requires

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     README = f.read()
 
 
-REQUIRES = ["cryptography", "chacha20poly1305-reuseable", "zeroconf>=0.36.2", "h11"]
+REQUIRES = ["cryptography", "chacha20poly1305-reuseable", "orjson>=3.7.2", "zeroconf>=0.36.2", "h11"]
 
 
 setup(
