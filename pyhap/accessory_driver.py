85140c121a25802205a208c7ce6b76a2d414937c
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-08-12 01:04:46 -0400

    Patch for [WinError 5] Access Denied

--- pyhap/accessory_driver.py
+++ pyhap/accessory_driver.py
@@ -641,6 +641,8 @@ class AccessoryDriver:
             ) as file_handle:
                 tmp_filename = file_handle.name
                 self.encoder.persist(file_handle, self.state)
+            if os.name == 'nt':  # Or `[WinError 5] Access Denied` will be raised on Windows
+                os.chmod(tmp_filename, 0o644)
             os.replace(tmp_filename, self.persist_file)
         finally:
             if tmp_filename and os.path.exists(tmp_filename):
